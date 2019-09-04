32ca2b0ea2f921fba2b12c8e1e9561db2ee4ca00
zhangshixun <zhangshixun@jjl.cn>
2019-09-04 18:07:45 +0800

    添加 element

--- increase/src/router/index.js
+++ increase/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import mainPage from '@/views/mainPage'
+import test1 from '@/views/page1/test1'
+import test2 from '@/views/page1/test2'
 
 Vue.use(Router)
 
@@ -16,6 +18,16 @@ export default new Router({
       path: '/main',
       name: 'mainPage',
       component: mainPage
+    },
+    {
+      path: '/test1',
+      name: 'test1',
+      component: test1
+    },
+    {
+      path: '/test2',
+      name: 'test2',
+      component: test2
     }
   ]
 })
